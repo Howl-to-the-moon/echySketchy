9ac617b1feddab9e5d81ef9445048a26d5fa1cce
Howl-to-the-moon <149625575+Howl-to-the-moon@users.noreply.github.com>
2025-11-10 15:46:55 -0500

    V0.2 random color comeplete
    implemented the thing to make a rainbow trail
    when you hover over the divs inside

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
     //Step 1. Created a 2x2 grid.
 
 
+        function getRandomColorRGB() {
+      const r = Math.floor(Math.random() * 256); // Random red value (0-255)
+      const g = Math.floor(Math.random() * 256); // Random green value (0-255)
+      const b = Math.floor(Math.random() * 256); // Random blue value (0-255)
+      return `rgb(${r},${g},${b})`;
+    }
+
     const container = document.querySelector(".container")
 
     
@@ -8,15 +15,22 @@
     for (let i = 0; i < 16 ; i++ ){
 
         const bread = document.createElement("div");
-        bread.classList.add("bread");
+
+        
             for (let i = 0; i < 16 ; i++){
 
                 const filling = document.createElement("div");
                 filling.classList.add("filling");
                 bread.appendChild(filling);
 
+                filling.addEventListener("mouseenter", () => {
+        filling.style.backgroundColor = getRandomColorRGB();
+    });
+
             };
 
         container.appendChild(bread);
 
     };
+
+    
