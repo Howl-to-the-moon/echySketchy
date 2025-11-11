5800bf6475cdea9028e3097d33a620c7792ec34a
Howl-to-the-moon <149625575+Howl-to-the-moon@users.noreply.github.com>
2025-11-11 15:34:14 -0500

    ok this isn;t working. this is gonna be the final commit for now and i think i'm just gonna tear it all down and try to start over. i did take a peek at someones completed code and i think i'm just doing too much

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-    //Step 1. Created a 2x2 grid.
+  
 
-//gets a random color. 
+//gets a random color. works, fine, nothing wrong with this
 
         function getRandomColorRGB() {
       const r = Math.floor(Math.random() * 256); // Random red value (0-255)
@@ -9,6 +9,11 @@
       return `rgb(${r},${g},${b})`;
     }
 
+
+/*i don't think anything is wrong with this one. 
+Basically it just gets the amount from the user, and calls itself again if they fuck up.
+the console logs only go off once so this isn't the part thats looping.
+*/
 
     function getAmount(){
 
@@ -28,13 +33,41 @@
 
     }
 
+
+
+
+/* 
+
+This is gonna have to be the part i destroy and bring back from scratch. 
+
+So Glombo is the main div thats in from the start in the html file. This is so we can destroy and recreate the container in it to reset the etchy sketch.
+
+we also have the resetB which is just targetting the reset button up top.
+
+
+
+so. we have a function etchSketch that takes the parameter square. square being the amount by amount of squares we want in the board.
+
+the first for loop creates the rows (horizontal) that will hold each etch a sketch square. this is so it makes each row so its a x by x board.
+
+the second for loop creates each square (filling) in each row (horizontal). making it x by x rows.
+
+maybe this is where i do the 960px / squares to make them propotional to the size or whatever???????????????
+
+then we append filling to horizontal each time. and also place the color changing div there
+
+
+
+*/
+
 const glombo = document.querySelector(".glombo");
 const resetB = document.querySelector("button");
 
     function etchSketch (square) {
 
-    const container = document.createElement("div")
-    container.classList.add("container");
+        const container = document.createElement("div")
+        container.classList.add("container");
+        glombo.appendChild(container);
 
     for (let i = 0; i < square ; i++ ){
 
@@ -45,6 +78,9 @@ const resetB = document.querySelector("button");
 
                 const filling = document.createElement("div");
                 filling.classList.add("filling");
+                filling.style.width = (960 / square);
+                filling.style.height = (960 / square);
+
                 horizontal.appendChild(filling);
 
                 filling.addEventListener("mouseenter", () => {
@@ -54,7 +90,7 @@ const resetB = document.querySelector("button");
             };
 
         container.appendChild(horizontal);
-        glombo.appendChild(container);
+        
 
     };
 
@@ -73,6 +109,7 @@ const resetB = document.querySelector("button");
 
     etchSketch(getAmount());
 
+    
     console.log("B");
 
 });
